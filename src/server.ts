368268daacc61e49c1667a06991252242cbd7471
scriptlver <sloowlaah1@gmail.com>
2025-07-07 20:40:43 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -23,11 +23,28 @@ const teams = [
 
 
 const drivers = [
-    { id: 1, name: "Lewis Hamilton", team: "Mercedes" },
-    { id: 2, name: "Charles Leclerc", team: "Ferrari" },
-    { id: 3, name: "Max Verstappen", team: "Red Bull Racing" },
-    { id: 4, name: "Lando Norris", team: "McLaren" }
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Sergio Pérez", team: "Red Bull Racing" },
+  { id: 3, name: "Lewis Hamilton", team: "Mercedes" },
+  { id: 4, name: "George Russell", team: "Mercedes" },
+  { id: 5, name: "Charles Leclerc", team: "Ferrari" },
+  { id: 6, name: "Carlos Sainz", team: "Ferrari" },
+  { id: 7, name: "Lando Norris", team: "McLaren" },
+  { id: 8, name: "Oscar Piastri", team: "McLaren" },
+  { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
+  { id: 10, name: "Lance Stroll", team: "Aston Martin" },
+  { id: 11, name: "Esteban Ocon", team: "Alpine" },
+  { id: 12, name: "Pierre Gasly", team: "Alpine" },
+  { id: 13, name: "Alex Albon", team: "Williams" },
+  { id: 14, name: "Logan Sargeant", team: "Williams" },
+  { id: 15, name: "Yuki Tsunoda", team: "Visa Cash App RB" },
+  { id: 16, name: "Daniel Ricciardo", team: "Visa Cash App RB" },
+  { id: 17, name: "Valtteri Bottas", team: "Stake F1 Team Kick Sauber" },
+  { id: 18, name: "Zhou Guanyu", team: "Stake F1 Team Kick Sauber" },
+  { id: 19, name: "Kevin Magnussen", team: "Haas" },
+  { id: 20, name: "Nico Hülkenberg", team: "Haas" }
 ];
+
 
 server.get("/teams", async (request, response) => {
   response.type("application/json").code(200);
@@ -40,6 +57,10 @@ server.get("/drivers", async (request, response) => {
 });
 
 interface DriversParams{
+    id: string;
+}
+
+interface TeamsParams{
     id: string;
 }
 
@@ -56,6 +77,20 @@ server.get<{Params: DriversParams}>("/drivers/:id", async (request, response) =>
   }
 
 });  
+
+server.get<{Params: TeamsParams}>("/teams/:id", async (request, response) => {
+  const id = parseInt(request.params.id);
+  const team = teams.find( t => t.id === id);
+  
+  if (!team){
+    response.type("application/json").code(404);
+    return { message: "Team not found" };
+  }else{
+    response.type("application/json").code(200);
+    return {team};
+  }
+
+});
 
 
 server.listen({port: 3333}, () => {
