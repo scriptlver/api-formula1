97e54ac9b83c4dab30d45833429a444c44326390
scriptlver <sloowlaah1@gmail.com>
2025-07-07 15:08:22 -0300

    ferrari

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,23 @@ const teams = [ { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
     { id: 2, name: "Ferrari", base: "Maranello, Italy" },
     { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
     { id: 4, name: "Mercedes", base: "Brackley, United Kingdom" }
-]
+];
+
+const drivers = [
+    { id: 1, name: "Lewis Hamilton", team: "Mercedes" },
+    { id: 2, name: "Charles Leclerc", team: "Ferrari" },
+    { id: 3, name: "Max Verstappen", team: "Red Bull Racing" },
+    { id: 4, name: "Lando Norris", team: "McLaren" }
+];
+
 server.get("/teams", async (request, response) => {
   response.type("application/json").code(200);
-  return [];
+  return [teams];
 });
 
 server.get("/drivers", async (request, response) => {
   response.type("application/json").code(200);
-  return {
-    id: 1,
-    name: "Max Verstappen",
-    team: "Red Bull Racing"
-  };
+  return [drivers];
 });
 
 
